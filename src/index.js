503d228aaf0dea81f4ff7a3f756407602368fa2c
Olga Pikulska <olga.pikulska.ld@gmail.com>
2022-12-03 18:25:55 +0100

    progress in style

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const searchInput = document.querySelector("input");
 const searchBtn = document.querySelector("button");
 const searchForm = document.querySelector("#search-form")
 const gallery = document.querySelector(".gallery");
-console.log(gallery)
+const loadMore = document.querySelector(".load-more")
+//const largeImgGallery = document.querySelector(".large-img-gallery")
+console.log(loadMore)
+
+let page = 1;
 
 searchForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
   e.preventDefault();
-  const inputValue = e.currentTarget.value;
+  //const inputValue = e.currentTarget.value;
   const searchQ = searchInput.value
-  console.log(inputValue);
+  //console.log(inputValue);
   console.log(searchQ)
   getImages(searchQ)
-  .then(friends => console.log(friends))
-  .catch(error => console.error(error));
+  
 }
 
 async function getImages(inputValue) {
@@ -29,25 +34,36 @@ async function getImages(inputValue) {
         image_type: "photo",
         orientation: "horizontal",
         safesearch: true,
+        page: page,
+        per_page: 40,
       }
     });
     console.log(response);
+    console.log(response.request.responseURL);
     const hits = response.data.hits
     console.log(hits);
-    renderImage(hits);
+    console.log(hits.length);
+    if (hits.length === 0) {
+      Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    } else {
+      Notify.success(`Hooray! We found ${response.data.totalHits} images.`)
+      renderImage(hits);
+    }
     
   } catch (error) {
     console.error(error);
   }
 }
 
-
-
 function renderImage(hits) {
   gallery.innerHTML = null;
+  //const markup = hits.map((hit) => {
+  //   const markupText = `<div class="photo-card"><a class="photo-card__img" href=${hit.largeImageURL}><img class="gallery__image" src=${hit.webformatURL} alt=${hit.tags} loading="lazy"/></a></div>`
+  //   return markupText;
+  // }).join("");
   const markup = hits.map((hit) => {
     const markupText = `<div class="photo-card">
-    <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" class="photo-card__item"/>
+    <a href=${hit.largeImageURL}><img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" class="photo-card__img"/></a>
     <div class="info">
     <p class="info-item">
     <b>Likes</b> ${hit.likes}
@@ -63,17 +79,37 @@ function renderImage(hits) {
     </p>
   </div>
     </div>`
-    return markupText
+    return markupText;
   }).join("");
   // nie mam pojęcia, czy będzie jakieś undefined//
   const markupReplaced = markup.replaceAll("undefined", "")
   gallery.innerHTML = markupReplaced;
+  let lightbox = new SimpleLightbox(".gallery a")
+}
+
+loadMore.addEventListener("click", handleClick);
+
+function handleClick() {
+  const searchQ = searchInput.value
+  page += 1;
+  getImages(searchQ);
 }
 
 
-
-
-
+{/* <div class="info">
+    <p class="info-item">
+    <b>Likes</b> ${hit.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${hit.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> ${hit.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${hit.downloads}
+    </p>
+  </div> */}
 
 //     axios.get('https://pixabay.com/api/', {
 //     params: {
