83c790ab96ec65c1a2923645a55492468b97e171
Olga Pikulska <olga.pikulska.ld@gmail.com>
2022-12-03 23:58:34 +0100

    load more

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+//czy wszystkie funkcje powinny być async/await?
+
 const searchInput = document.querySelector("input");
-const searchBtn = document.querySelector("button");
 const searchForm = document.querySelector("#search-form")
 const gallery = document.querySelector(".gallery");
 const loadMore = document.querySelector(".load-more")
-//const largeImgGallery = document.querySelector(".large-img-gallery")
-console.log(loadMore)
+
+//loadMore.setAttribute("hidden","")
 
 let page = 1;
 
 searchForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
+  gallery.innerHTML = null;
   e.preventDefault();
-  //const inputValue = e.currentTarget.value;
+  page = 1;
   const searchQ = searchInput.value
-  //console.log(inputValue);
-  console.log(searchQ)
   getImages(searchQ)
   
 }
@@ -41,29 +41,31 @@ async function getImages(inputValue) {
     console.log(response);
     console.log(response.request.responseURL);
     const hits = response.data.hits
-    console.log(hits);
-    console.log(hits.length);
+    console.log(hits)
     if (hits.length === 0) {
       Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-    } else {
+    } else if (page === 1) {
       Notify.success(`Hooray! We found ${response.data.totalHits} images.`)
-      renderImage(hits);
     }
+    renderImage(hits);
     
+    const totalHits = response.data.totalHits;
+    const numberOfPages = Math.ceil(totalHits / 40);
+    console.log(numberOfPages)
+    
+    if (page  === numberOfPages) {
+      loadMore.setAttribute("hidden", "")
+      Notify.failure("We're sorry, but you've reached the end of search results.")
+    }
   } catch (error) {
     console.error(error);
   }
 }
 
 function renderImage(hits) {
-  gallery.innerHTML = null;
-  //const markup = hits.map((hit) => {
-  //   const markupText = `<div class="photo-card"><a class="photo-card__img" href=${hit.largeImageURL}><img class="gallery__image" src=${hit.webformatURL} alt=${hit.tags} loading="lazy"/></a></div>`
-  //   return markupText;
-  // }).join("");
   const markup = hits.map((hit) => {
     const markupText = `<div class="photo-card">
-    <a href=${hit.largeImageURL}><img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" class="photo-card__img"/></a>
+    <a class="photo-card__link" href=${hit.largeImageURL}><img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" class="photo-card__img"/></a>
     <div class="info">
     <p class="info-item">
     <b>Likes</b> ${hit.likes}
@@ -81,10 +83,10 @@ function renderImage(hits) {
     </div>`
     return markupText;
   }).join("");
-  // nie mam pojęcia, czy będzie jakieś undefined//
-  const markupReplaced = markup.replaceAll("undefined", "")
-  gallery.innerHTML = markupReplaced;
+  gallery.insertAdjacentHTML("beforeend", markup)
+  loadMore.removeAttribute("hidden")
   let lightbox = new SimpleLightbox(".gallery a")
+  lightbox.refresh();
 }
 
 loadMore.addEventListener("click", handleClick);
@@ -93,66 +95,16 @@ function handleClick() {
   const searchQ = searchInput.value
   page += 1;
   getImages(searchQ);
+
+  // sprawdzić czy to wgl działa 
+  const { height: cardHeight } = gallery
+  .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
 }
 
 
-{/* <div class="info">
-    <p class="info-item">
-    <b>Likes</b> ${hit.likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b> ${hit.views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b> ${hit.comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b> ${hit.downloads}
-    </p>
-  </div> */}
-
-//     axios.get('https://pixabay.com/api/', {
-//     params: {
-//         key:"31759222-00acf71bf0a65e43bd085eba1",
-//         q:"${searchQ}",
-//         image_type: "photo",
-//         orientattion: "horizontal",
-//         safesearch:true,
-//     }
-//   })
-//     .then(function (response) {
-//     console.log(typeof response)
-//     console.log(response);
-//     })
-//   .catch(function (error) {
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // always executed
-//   });  
-// }
-
-// searchInput.addEventListener("input", (e) => {
-// let inputValue = e.currentTarget.value
-//     console.log(inputValue);
-//     axios.get('https://pixabay.com/api/', {
-//     params: {
-//         key:"31759222-00acf71bf0a65e43bd085eba1",
-//         q:"${inputValue}",
-//         image_type: "photo",
-//         orientattion: "horizontal",
-//         safesearch:true,
-//     }
-//   })
-//         .then(function (response) {
-//       console.log(typeof response)
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // always executed
-//   });  
-// })
-
+// ZOSTAŁO: przewijanie strony i nieskończone przewijanie i poprawić I'm sorry you've reach
